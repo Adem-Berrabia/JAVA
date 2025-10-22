5faf4ba7046068ab2e3f3eac04565e4a1d0b3708
adem rb <adamrabia79@gmail.com>
2025-10-22 12:10:12 +0100

    Refactor point classes and main logic; remove unused Point1, Point2, Point3 classes and implement alignment check in Point class.

--- ex2/src/Main/Main.java
+++ ex2/src/Main/Main.java
@@ -2,42 +2,17 @@ package Main;
 
 public class Main {
     public static void main(String[] args) {
-        // Points 1D
-        Point1 p1 = new Point1(3);
-        Point1 p2 = new Point1(7);
-        p1.afficher();
-        System.out.println("Distance 1D = " + p1.distance(p2));
-        p1.deplacer(2);
-        p1.afficher();
-
-        System.out.println("---------------------");
-
-        // Points 2D
-        Point2 A = new Point2(1, 2);
-        Point2 B = new Point2(3, 4);
-        Point2 C = new Point2(5, 6);
-        A.afficher();
-        B.afficher();
-        C.afficher();
-
-        System.out.println("Distance AB = " + A.distance(B));
-        System.out.println("A, B, C alignés ? " + Point2.alignes(A, B, C));
-        A.deplacer(1, -1);
-        A.afficher();
-
-        System.out.println("---------------------");
-
-        // Points 3D
-        Point3 X = new Point3(1, 2, 3);
-        Point3 Y = new Point3(2, 3, 4);
-        Point3 Z = new Point3(3, 4, 5);
-        X.afficher();
-        Y.afficher();
-        Z.afficher();
-
-        System.out.println("Distance XY = " + X.distance(Y));
-        System.out.println("X, Y, Z alignés ? " + Point3.alignes(X, Y, Z));
-        X.deplacer(1, 1, 1);
-        X.afficher();
+        Point p1 = new Point(1,1);
+        Point p2 = new Point(2,2);
+        Point p3 = new Point(3,3);
+        // int D=p1.distance(p2);
+        // System.out.println("Distance between p1 and p2: " + D);
+        Boolean t = p1.Alignement(p2,p3);
+        if (t) {
+            System.out.println("The points are aligned");
+        } else {
+            System.out.println("The points are not aligned");
+            
+        }
     }
 }
---
+++ ex2/src/Main/Point.java
@@ -0,0 +1,22 @@
+package Main;
+
+public class Point {
+    private int x, y, z;
+    public Point(int x,int y) {
+        this.x = x;
+        this.y = y;
+    }
+    public int getX() {
+        return x;
+    }
+    public void setx(int a) {
+        x = a;
+    }
+    // Alignement 
+    public Boolean Alignement(Point p2, Point p3) {
+        if (((x-p2.x)/(x-p3.x))==((y-p2.y)/(y-p3.y))) {
+            return true;
+        }
+        return false;
+    }
+}
